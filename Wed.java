14ba6d00225634a07a673e523c271eb3c0358add
breadjoo <167046643+breadjoo@users.noreply.github.com>
2024-04-15 15:26:06 +0900

    Add files via upload

---
+++ Wed.java
@@ -0,0 +1,21 @@
+package ThisisJava;
+
+public class Wed {
+
+	public static void main(String[] args) {
+ System.out.println("Wednesday ");
+ System.out.println("시스템.out.println");
+ System.out.println("System의 시작은 대문자로 해야함");
+ System.out.println("처음거가 대문자가 아니면 오류");
+ 
+ System.out.print("띄어쓰기 없이 ");
+ System.out.print("가 아니고 엔터없이  ");
+
+ System.out.printf("가나다라");  
+	
+ System.out.println(010);
+ System.out.println(0xff);
+ System.out.println("O________o;");
+	}
+
+}
