0b5f374cfbc113ffb1866c50c177bd400cdae8a5
breadjoo <167046643+breadjoo@users.noreply.github.com>
2024-05-26 23:34:27 +0900

    Add files via upload

---
+++ S0526/exam01/Main.java
@@ -0,0 +1,20 @@
+package S0526.exam01;
+
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+
+		Scanner scanner = new Scanner(System.in);
+
+        int A = scanner.nextInt();
+        int B = scanner.nextInt();
+
+        int result = A - B;
+        System.out.println(result);
+
+   
+	}
+
+}
---
+++ S0526/exam01/Main2.java
@@ -0,0 +1,23 @@
+package S0526.exam01;
+
+import java.util.Scanner;
+
+public class Main2 {
+
+	public static void main(String[] args) {
+
+		Scanner scanner = new Scanner(System.in);
+
+		        int A = scanner.nextInt();
+		        int B = scanner.nextInt();
+
+		        int result = A * B;
+		        System.out.println(result);
+
+		   
+			}
+
+		}
+
+
+
